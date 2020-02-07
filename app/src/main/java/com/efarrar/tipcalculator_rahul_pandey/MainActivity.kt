426f0bd2946ae97b3a08930d6e27bd22d21eaf8b
Emmanuel Farrar <6923873+emmanuelfarrar@users.noreply.github.com>
2020-02-07 04:21:39 +0300

    Initial commit

---
+++ app/src/main/java/com/efarrar/tipcalculator_rahul_pandey/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.efarrar.tipcalculator_rahul_pandey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
